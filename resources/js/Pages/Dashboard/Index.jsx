7847776d5375f04441fbc71b05c1f995fd970faa
mhazrla <mhazrla@gmail.com>
2023-02-24 14:16:47 +0700

    add component navbar

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link, Head } from "@inertiajs/react";
+import Sidebar from "@/Components/Sidebar";
+import Navbar from "@/Components/Navbar";
 
 export default function Index(props) {
     console.log(props);
     return (
         <div>
             <Head title={props.title} />
+            <Navbar />
+
             {props.tools
                 ? props.tools.map((data, i) => {
                       return <p key={i}>{data.alatId}</p>;
